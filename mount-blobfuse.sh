fa983c69e417ba3fa13cb244980a3f08ec72a12f
Nik Katalnikov <nik.katalnikov@icloud.com>
2021-03-03 15:27:21 +0200

    Docker compose quick fix

--- mount-blobfuse.sh
+++ mount-blobfuse.sh
@@ -7,8 +7,8 @@ IFS=$'\n\t'
 
 # mount our blobstore
 test ${AZURE_MOUNT_POINT}
-rm -rf ${AZURE_MOUNT_POINT}
-mkdir -p ${AZURE_MOUNT_POINT}
+# rm -rf ${AZURE_MOUNT_POINT}
+# mkdir -p ${AZURE_MOUNT_POINT}
 
 blobfuse ${AZURE_MOUNT_POINT} --use-https=true --tmp-path=/tmp/blobfuse/${AZURE_STORAGE_ACCOUNT} --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
 
